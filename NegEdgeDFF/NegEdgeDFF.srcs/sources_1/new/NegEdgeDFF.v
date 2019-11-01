ab55bfc3abe1c535d18886f5c111dbc10ab0afc0
ariyonaty <ari.h.yonaty@gmail.com>
2019-11-01 16:11:38 -0700

    Added Falling Edge D Flip Flop.

---
+++ NegEdgeDFF/NegEdgeDFF.srcs/sources_1/new/NegEdgeDFF.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/31/2019 09:45:24 PM
+// Design Name: 
+// Module Name: NegEdgeDFF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module NegEdgeDFF(
+    input       D,
+    input       clk,
+    output reg  Qm,
+    output reg  Qs
+    );
+    
+    always @ (D or clk) begin
+        if (clk)
+            Qm = D;
+    end
+    
+    always @ (negedge clk) begin
+        Qs = Qm;
+    end
+    
+endmodule
+    
