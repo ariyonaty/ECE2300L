329c2724a6b352c5e7e968285e0383f92c106b8c
ariyonaty <ari.h.yonaty@gmail.com>
2019-12-01 00:49:00 -0800

    Added overlay to Pattern Detect.

--- Final/PatternDetect/PatternDetect.srcs/sources_1/new/seq_detect.v
+++ Final/PatternDetect/PatternDetect.srcs/sources_1/new/seq_detect.v
@@ -30,14 +30,15 @@ module seq_detect(seq_in, clk, reset, state, out);
     
     reg [7:0] state, next_state;
     
-    parameter   S0 = 8'b00000001,
-                S1 = 8'b00000010,
-                S2 = 8'b00000100,
-                S3 = 8'b00001000,
-                S4 = 8'b00010000,
-                S5 = 8'b00100000,
-                S6 = 8'b01000000,
-                S7 = 8'b10000000;
+    parameter   S0 = 8'b00000000,
+                S1 = 8'b00000001,
+                S2 = 8'b00000010,
+                S3 = 8'b00000100,
+                S4 = 8'b00001000,
+                S5 = 8'b00010000,
+                S6 = 8'b00100000,
+                S7 = 8'b01000000,
+                S8 = 8'b10000000;
     
     always @ (posedge clk or posedge reset)
         if (reset) begin
@@ -52,18 +53,19 @@ module seq_detect(seq_in, clk, reset, state, out);
             S1: if (seq_in) next_state <= S0;  else next_state <= S2;
             S2: if (seq_in) next_state <= S0;  else next_state <= S3;
             S3: if (seq_in) next_state <= S4;  else next_state <= S0;
-            S4: if (seq_in) next_state <= S0;  else next_state <= S5;
-            S5: if (seq_in) next_state <= S6;  else next_state <= S0;
-            S6: if (seq_in) next_state <= S7;  else next_state <= S0;
-            S7: if (seq_in) next_state <= S0;  else next_state <= S0;
+            S4: if (seq_in) next_state <= S1;  else next_state <= S5;
+            S5: if (seq_in) next_state <= S6;  else next_state <= S3;
+            S6: if (seq_in) next_state <= S7;  else next_state <= S2;
+            S7: if (seq_in) next_state <= S0;  else next_state <= S8;
+            S8: if (seq_in) next_state <= S0;  else next_state <= S3; 
             default: next_state <= S0;
         endcase        
          
         
     always @  (*)
         case (state)
-            S0, S1, S2, S3, S4, S5, S6: out <= 1'b0;
-                                    S7: out <= 1'b1;
+            S0, S1, S2, S3, S4, S5, S6, S7: out <= 1'b0;
+                                        S8: out <= 1'b1;
             default: out <= 1'b0;
         endcase
           
